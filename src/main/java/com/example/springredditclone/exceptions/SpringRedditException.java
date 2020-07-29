b17e4fa3e0a6ae89ea769b253bdef6e0a7ceaa00
zmiles17 <zgm_striker17@hotmail.com>
2020-07-29 11:47:27 -0400

    everything so far

---
+++ src/main/java/com/example/springredditclone/exceptions/SpringRedditException.java
@@ -0,0 +1,7 @@
+package com.example.springredditclone.exceptions;
+
+public class SpringRedditException extends RuntimeException {
+    public SpringRedditException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/springredditclone/repository/RefreshTokenRepository.java
@@ -0,0 +1,8 @@
+package com.example.springredditclone.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenRepository {
+}
